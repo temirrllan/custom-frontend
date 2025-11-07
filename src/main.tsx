f7c7af539d8068a2cfd0f5152464078094a672c7
Temirlan <raiymbektemirlan25@gmail.com>
2025-11-07 13:35:54 +0500

    style

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import WebApp from "@twa-dev/sdk";
+import "./index.css";
+import "./App.css";
 import "./styles/app.css";
 
 WebApp.ready();
